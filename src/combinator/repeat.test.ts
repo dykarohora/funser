8e07f75f7f2c54ae9b0d87c6e9f2c45382f925ac
d_yama <7d.yama@gmail.com>
2023-08-22 01:13:22 +0900

    [update]repeatコンビネータのテストを追加した

--- src/combinator/repeat.test.ts
+++ src/combinator/repeat.test.ts
@@ -1,8 +1,65 @@
 import { pipe } from './pipe.js'
 import { string } from '../parser/string.js'
 import { repeat } from './repeat.js'
+import { or } from './or.js'
 
 describe('repeat', () => {
+	describe('他のコンビネータと組み合わせることができる', () => {
+		const parser = pipe(
+			string('hi'),
+			or(string('hey')),
+			repeat({ max: 2 }),
+		)
+
+		it('入力がhihiのときは、パースに成功し結果として["hi", "hi"]を取得できる', () => {
+			const input = 'hihi'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['hi', 'hi'])
+			expect(output.state.position).toEqual(4)
+		})
+
+		it('入力がhiheyhiのときは、パースに成功し結果として["hi", "hey"]を取得できる', () => {
+			const input = 'hiheyhi'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['hi', 'hey'])
+			expect(output.state.position).toEqual(5)
+		})
+
+		it('入力がheyheyのときは、パースに成功し結果として["hey", "hey"]を取得できる', () => {
+			const input = 'heyhey'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['hey', 'hey'])
+			expect(output.state.position).toEqual(6)
+		})
+
+		it('入力がheyhiheyのときは、パースに成功し結果として["hey", "hi"]を取得できる', () => {
+			const input = 'heyhihey'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['hey', 'hi'])
+			expect(output.state.position).toEqual(5)
+		})
+	})
+
 	describe('下限が設定されている', () => {
 		const parser = pipe(
 			string('hi'),
@@ -14,6 +71,7 @@ describe('repeat', () => {
 			const output = parser({ input })
 
 			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
 		})
 
 		it('入力がhiのときは、パースに失敗する', () => {
@@ -21,6 +79,7 @@ describe('repeat', () => {
 			const output = parser({ input })
 
 			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
 		})
 
 		it('入力がhihiのときは、パースに成功し結果として["hi", "hi"]を取得できる', () => {
@@ -114,6 +173,7 @@ describe('repeat', () => {
 			const output = parser({ input })
 
 			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
 		})
 
 		it('入力がhiのときは、パースに成功し結果として["hi"]を取得できる', () => {
