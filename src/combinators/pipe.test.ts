65a10d80c4d2d872fa2130b1e73e4245a37c0e94
d_yama <7d.yama@gmail.com>
2023-08-21 21:18:51 +0900

    [add]コンビネータをパイプ接続する関数を実装した

--- src/combinators/pipe.test.ts
+++ src/combinators/pipe.test.ts
@@ -1,36 +1,94 @@
-import { pipe } from './pipe.js'
 import { string } from '../parser/string.js'
 import { seq } from './seq.js'
-import { Parser } from '../parser/types.js'
+import { seq as sequential } from '../parser/seq.js'
 import { or } from './or.js'
+import { or as alternative } from '../parser/or.js'
+import { pipe } from './pipe.js'
+import { Parser } from '../parser/types.js'
 
 describe('pipe', () => {
+	it('引数が一つのとき、引数に渡したパーサがそのまま返ってくる', () => {
+		const parser = pipe(
+			string('hello')
+		)
+
+		const output = parser({ input: 'hello' })
+		if (output.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output.value).toEqual('hello')
+	})
+
+	it('一つのコンビネータを適用できる', () => {
+		const parser = pipe(
+			string('hello'),
+			or(string('good bye'))
+		)
+
+		const output1 = parser({ input: 'hello' })
+		if (output1.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output1.value).toEqual('hello')
+
+		const output2 = parser({ input: 'good bye' })
+		if (output2.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output2.value).toEqual('good bye')
+	})
+
+	it('二つのコンビネータを適用できる', () => {
+		const parser = pipe(
+			alternative(string('hello '), string('good bye ')),
+			seq(alternative(string('cat'), string('dog'))),
+			seq(string('!!'))
+		)
+
+		const output1 = parser({ input: 'hello cat!!' })
+		if (output1.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output1.value).toEqual([['hello ', 'cat'], '!!'])
+
+		const output2 = parser({ input: 'good bye dog!!' })
+		if (output2.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output2.value).toEqual([['good bye ', 'dog'], '!!'])
+	})
+
 	it('test1', () => {
 		const parser = pipe(
 			string('hello '),
 			seq(string('world'), string('!!'))
 		)
 
-		const output = parser({input: 'hello world!!'})
-		if(output.type === 'Failure'){
+		const output = parser({ input: 'hello world!!' })
+		if (output.type === 'Failure') {
 			throw new Error('test failed')
 		}
 
-		expect(output.value).toEqual(['hello ', ['world', '!!']])
+		expect(output.value).toEqual(['hello ', 'world', '!!'])
 		expect(output.state.position).toEqual(13)
 	})
 
 	it('test2', () => {
 		const parser = pipe(
-			seq(string('Good '), string('morning'), string('!! ')),
+			sequential(string('Good '), string('morning'), string('!! ')),
 			seq(string('Good '), string('afternoon'), string('!! ')),
 		)
-		const output = parser({input: 'Good morning!! Good afternoon!! '})
-		if(output.type === 'Failure'){
+		const output = parser({ input: 'Good morning!! Good afternoon!! ' })
+		if (output.type === 'Failure') {
 			throw new Error('test failed')
 		}
 
-		expect(output.value).toEqual([['Good ', 'morning', '!! '], ['Good ', 'afternoon', '!! ']])
+		expect(output.value).toEqual([['Good ', 'morning', '!! '], 'Good ', 'afternoon', '!! '])
 		expect(output.state.position).toEqual(32)
 	})
 
@@ -40,20 +98,45 @@ describe('pipe', () => {
 			or(string('world'))
 		)
 
-		const output1 = parser({input: 'hello'})
-		if(output1.type === 'Failure'){
+		const output1 = parser({ input: 'hello' })
+		if (output1.type === 'Failure') {
 			throw new Error('test failed')
 		}
 
 		expect(output1.value).toEqual('hello')
 		expect(output1.state.position).toEqual(5)
 
-		const output2 = parser({input: 'world'})
-		if(output2.type === 'Failure'){
+		const output2 = parser({ input: 'world' })
+		if (output2.type === 'Failure') {
 			throw new Error('test failed')
 		}
 
 		expect(output2.value).toEqual('world')
 		expect(output2.state.position).toEqual(5)
+	})
+
+	it('test4', () => {
+		const parser2 = pipe(
+			string('hello'),
+			or(string('world'))
+		)
+
+		const parser3 = pipe(
+			string('a'),
+			seq(string('b')),
+		)
+
+		const parser = pipe(
+			string('a'),
+			seq(string('b')),
+			seq(string('c'))
+		)
+
+		const output1 = parser({ input: 'abc' })
+		if (output1.type === 'Failure') {
+			throw new Error('test failed')
+		}
+
+		expect(output1.value).toEqual([['a', 'b'], 'c'])
 	})
 })
--- src/combinators/pipe.ts
+++ src/combinators/pipe.ts
@@ -1,17 +1,19 @@
 import type { Combinator, Parser } from '../parser/types.js'
-import { seq } from './seq.js'
-import { or } from './or.js'
 
-export const pipe = <T, P extends Array<Parser<unknown>>>(
-	parser: Parser<T>,
-	combinator: Combinator<P>
-) => {
-	switch (combinator.type) {
-		case 'or':
-			return or(parser, combinator)
-		case 'seq':
-			return seq(parser, combinator)
-		default:
-			throw new Error(combinator satisfies never)
-	}
+export function pipe<T1>(sourceParser: Parser<T1>): Parser<T1>
+export function pipe<T1, T2>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>): Parser<T2>
+export function pipe<T1, T2, T3>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>): Parser<T3>
+export function pipe<T1, T2, T3, T4>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>): Parser<T4>
+export function pipe<T1, T2, T3, T4, T5>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>): Parser<T5>
+export function pipe<T1, T2, T3, T4, T5, T6>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>): Parser<T6>
+export function pipe<T1, T2, T3, T4, T5, T6, T7>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>, c6: Combinator<T6, T7>): Parser<T7>
+export function pipe<T1, T2, T3, T4, T5, T6, T7, T8>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>, c6: Combinator<T6, T7>, c7: Combinator<T7, T8>): Parser<T8>
+export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>, c6: Combinator<T6, T7>, c7: Combinator<T7, T8>, c8: Combinator<T8, T9>): Parser<T9>
+export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>, c6: Combinator<T6, T7>, c7: Combinator<T7, T8>, c8: Combinator<T8, T9>, c9: Combinator<T9, T10>): Parser<T10>
+export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(sourceParser: Parser<T1>, c1: Combinator<T1, T2>, c2: Combinator<T2, T3>, c3: Combinator<T3, T4>, c4: Combinator<T4, T5>, c5: Combinator<T5, T6>, c6: Combinator<T6, T7>, c7: Combinator<T7, T8>, c8: Combinator<T8, T9>, c9: Combinator<T9, T10>, c10: Combinator<T10, T11>): Parser<T11>
+export function pipe(
+	sourceParser: Parser<unknown>,
+	...combinators: Array<Combinator<unknown, unknown>>
+) {
+	return combinators.reduce((acc, combinator) => combinator(acc), sourceParser)
 }
