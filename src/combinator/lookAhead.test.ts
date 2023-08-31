d51a7d3a9470a3cbd17255a1b37005e398ef50f2
d_yama <7d.yama@gmail.com>
2023-08-31 17:04:21 +0900

    [update]lookAheadコンビネータに入力を消費するかどうかのオプションを追加した

--- src/combinator/lookAhead.test.ts
+++ src/combinator/lookAhead.test.ts
@@ -3,35 +3,95 @@ import { string } from '../parser/string.js'
 import { lookAhead } from './lookAhead.js'
 
 describe('lookAhead', () => {
-	const parser = pipe(
-		string('hello'),
-		lookAhead(string('world')),
-	)
+	describe('consumption', () => {
+		const parser = pipe(
+			string('hello'),
+			lookAhead(string('world')),
+		)
 
-	it('入力が空のときは、パースに失敗する', () => {
-		const input = ''
-		const output = parser({ input })
+		it('入力が空のときは、パースに失敗する', () => {
+			const input = ''
+			const output = parser({ input })
 
-		expect(output.type).toEqual('Failure')
+			expect(output.type).toEqual('Failure')
+		})
+
+		it('入力が"helloworld"のとき、パースに成功し結果として"hello"が取得できる', () => {
+			const input = 'helloworld'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual('hello')
+			expect(output.state.position).toEqual(10)
+		})
+
+		it('入力が"helloworld"でパースの開始位置が1のとき、パースに成功し結果として"hello"が取得できる', () => {
+			const input = 'ahelloworld'
+			const output = parser({ input, position: 1 })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual('hello')
+			expect(output.state.position).toEqual(11)
+		})
+
+		it('入力が"hellohey"のとき、パースに失敗する', () => {
+			const input = 'hellohey'
+			const output = parser({ input })
+
+			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
+		})
 	})
 
-	it('入力が"helloworld"のとき、パースに成功し結果として"hello"が取得できる', () => {
-		const input = 'helloworld'
-		const output = parser({ input })
+	describe('no consumption', () => {
+		const parser = pipe(
+			string('hello'),
+			lookAhead(string('world'), { consumption: false }),
+		)
 
-		if (output.type === 'Failure') {
-			throw new Error('test failed')
-		}
+		it('入力が空のときは、パースに失敗する', () => {
+			const input = ''
+			const output = parser({ input })
 
-		expect(output.value).toEqual('hello')
-		expect(output.state.position).toEqual(10)
-	})
+			expect(output.type).toEqual('Failure')
+		})
 
-	it('入力が"hellohey"のとき、パースに失敗する', () => {
-		const input = 'hellohey'
-		const output = parser({ input })
+		it('入力が"ahelloworld"でパース開始位置が1のとき、パースに成功し結果として"hello"が取得でき、positionは5となる', () => {
+			const input = 'ahelloworld'
+			const output = parser({ input, position: 1 })
 
-		expect(output.type).toEqual('Failure')
-		expect(output.state.position).toEqual(0)
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual('hello')
+			expect(output.state.position).toEqual(6)
+		})
+
+		it('入力が"helloworld"のとき、パースに成功し結果として"hello"が取得でき、positionは5となる', () => {
+			const input = 'helloworld'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual('hello')
+			expect(output.state.position).toEqual(5)
+		})
+
+		it('入力が"hellohey"のとき、パースに失敗する', () => {
+			const input = 'hellohey'
+			const output = parser({ input })
+
+			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
+		})
 	})
 })
--- src/combinator/lookAhead.ts
+++ src/combinator/lookAhead.ts
@@ -3,13 +3,39 @@ import { pipe } from './pipe.js'
 import { seqParser } from '../parser/index.js'
 import { map } from './map.js'
 
+type Option = {
+	consumption?: boolean
+}
+
 /**
  * 前段のパーサと後段のパーサの両方が成功したとき、前段の結果を返すコンビネータ
  */
 export const lookAhead =
-	<T, U>(second: Parser<T>) =>
+	<T, U>(second: Parser<T>, option: Option = { consumption: true }) =>
 		(first: Parser<U>): Parser<U> =>
-			pipe(
-				seqParser(first, second),
-				map(([first, _]) => first)
-			)
+			({ input, position = 0 }) => {
+				const { consumption = true } = option
+				if (consumption) {
+					return pipe(
+						seqParser(first, second),
+						map(([firstResult,]) => firstResult)
+					)({ input, position })
+				}
+
+				const firstResult = first({ input, position })
+				if (firstResult.type === 'Failure') {
+					return firstResult
+				}
+
+				const secondResult = second({ input, position: firstResult.state.position })
+
+				return secondResult.type === 'Success'
+					? {
+						...firstResult,
+						state: { input, position: firstResult.state.position }
+					}
+					: {
+						...secondResult,
+						state: { input, position }
+					}
+			}
