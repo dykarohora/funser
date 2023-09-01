ad3b38d65731e5458273cf3e69fcacc3df32a44f
d_yama <7d.yama@gmail.com>
2023-09-01 11:55:59 +0900

    [update]repeatTillで入力を消費するかどうか選択するオプションを追加した

--- src/combinator/repeatTill.test.ts
+++ src/combinator/repeatTill.test.ts
@@ -3,60 +3,105 @@ import { anyChar, anyCharOf } from '../parser/index.js'
 import { repeatTill } from './repeatTill.js'
 
 describe('repeatTill', () => {
-	const parser = pipe(
-		anyChar,
-		repeatTill(anyCharOf('!'))
-	)
+	describe('オプションなし', () => {
+		const parser = pipe(
+			anyChar,
+			repeatTill(anyCharOf('!'))
+		)
 
-	it('入力が空文字の場合は、パースに失敗する', () => {
-		const input = ''
-		const output = parser({ input })
+		it('入力が空文字の場合は、パースに失敗する', () => {
+			const input = ''
+			const output = parser({ input })
 
-		expect(output.type).toEqual('Failure')
-		expect(output.state.position).toEqual(0)
+			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
+		})
+
+		it('入力が"!"の場合は、パースに成功し結果として["!"]を取得できる', () => {
+			const input = '!'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['!'])
+			expect(output.state.position).toEqual(1)
+		})
+
+		it('入力が"abc"の場合は、パースに失敗する', () => {
+			const input = 'abc'
+			const output = parser({ input })
+
+			expect(output.type).toEqual('Failure')
+			expect(output.state.position).toEqual(0)
+		})
+
+		it('入力が"abc!"の場合は、パースに成功し結果として["a", "b", "c", "!"]を取得できる', () => {
+			const input = 'abc!'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['a', 'b', 'c', '!'])
+			expect(output.state.position).toEqual(4)
+		})
+
+		it('入力が"abc!def"の場合は、パースに成功し結果として["a", "b", "c", "!"]を取得できる', () => {
+			const input = 'abc!def'
+			const output = parser({ input })
+
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
+
+			expect(output.value).toEqual(['a', 'b', 'c', '!'])
+			expect(output.state.position).toEqual(4)
+		})
 	})
 
-	it('入力が"!"の場合は、パースに成功し結果として["!"]を取得できる', () => {
-		const input = '!'
-		const output = parser({ input })
+	describe('consumption: false', () => {
+		const parser = pipe(
+			anyChar,
+			repeatTill(anyCharOf('!'), { consumption: false })
+		)
 
-		if (output.type === 'Failure') {
-			throw new Error('test failed')
-		}
+		it('入力が"!"の場合は、パースに成功し次の開始位置は0となる', () => {
+			const input = '!'
+			const output = parser({ input })
 
-		expect(output.value).toEqual(['!'])
-		expect(output.state.position).toEqual(1)
-	})
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
 
-	it('入力が"abc"の場合は、パースに失敗する', () => {
-		const input = 'abc'
-		const output = parser({ input })
+			expect(output.value).toEqual(['!'])
+			expect(output.state.position).toEqual(0)
+		})
 
-		expect(output.type).toEqual('Failure')
-		expect(output.state.position).toEqual(0)
-	})
+		it('入力が"abc!"の場合は、パースに成功し次の開始位置は3となる', () => {
+			const input = 'abc!'
+			const output = parser({ input })
 
-	it('入力が"abc!"の場合は、パースに成功し結果として["a", "b", "c", "!"]を取得できる', () => {
-		const input = 'abc!'
-		const output = parser({ input })
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
 
-		if (output.type === 'Failure') {
-			throw new Error('test failed')
-		}
+			expect(output.value).toEqual(['a', 'b', 'c', '!'])
+			expect(output.state.position).toEqual(3)
+		})
 
-		expect(output.value).toEqual(['a', 'b', 'c', '!'])
-		expect(output.state.position).toEqual(4)
-	})
+		it('入力が"abc!def"の場合は、パースに成功し次の開始位置は3となる', () => {
+			const input = 'abc!def'
+			const output = parser({ input })
 
-	it('入力が"abc!def"の場合は、パースに成功し結果として["a", "b", "c", "!"]を取得できる', () => {
-		const input = 'abc!def'
-		const output = parser({ input })
+			if (output.type === 'Failure') {
+				throw new Error('test failed')
+			}
 
-		if (output.type === 'Failure') {
-			throw new Error('test failed')
-		}
-
-		expect(output.value).toEqual(['a', 'b', 'c', '!'])
-		expect(output.state.position).toEqual(4)
+			expect(output.value).toEqual(['a', 'b', 'c', '!'])
+			expect(output.state.position).toEqual(3)
+		})
 	})
 })
--- src/combinator/repeatTill.ts
+++ src/combinator/repeatTill.ts
@@ -2,30 +2,37 @@ import type { Parser } from '../types/index.js'
 
 // TODO オプションどうする？
 // tillの結果を含めるかどうか
-// 入力を消費するかどうか
+// tillに入力を消費するかどうか
+
+type Option = {
+	consumption?: boolean // 引数tillに渡したパーサが入力を消費するかどうか
+}
 
 /**
  * 指定したパーサによるパースが成功するまで、前段のパーサを繰り返し実行するコンビネータ
  * @param till
+ * @param option
  */
 export const repeatTill =
-	<T, U>(till: Parser<U>) =>
+	<T, U>(till: Parser<U>, option: Option = { consumption: true }) =>
 		(parser: Parser<T>): Parser<[...T[], U]> =>
 			({ input, position = 0 }) => {
+				const { consumption = true } = option
 				const values: T[] = []
 
-				for (let currentPos = position; ;) {
-					const tillOutput = till({ input, position: currentPos })
+				let state = { input, position }
+				for (; ;) {
+					const tillOutput = till({ input, position: state.position })
 
 					if (tillOutput.type === 'Success') {
 						return {
 							type: 'Success',
 							value: [...values, tillOutput.value],
-							state: tillOutput.state
+							state: consumption ? tillOutput.state : state
 						}
 					}
 
-					const parserOutput = parser({ input, position: currentPos })
+					const parserOutput = parser({ input, position: state.position })
 
 					if (parserOutput.type === 'Failure') {
 						return {
@@ -36,6 +43,6 @@ export const repeatTill =
 					}
 
 					values.push(parserOutput.value)
-					currentPos = parserOutput.state.position
+					state = parserOutput.state
 				}
 			}
