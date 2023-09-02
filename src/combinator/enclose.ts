13985595a7767b403919cd626d84f9338d08471b
d_yama <7d.yama@gmail.com>
2023-09-03 01:09:49 +0900

    encloseコンビネータをvalidateコンビネータを使って描き直した

--- src/combinator/enclose.ts
+++ src/combinator/enclose.ts
@@ -1,4 +1,8 @@
 import type { Parser, ParserOutput } from '../types/index.js'
+import { seqParser } from '../parser/index.js'
+import { pipe } from './pipe.js'
+import { subtract } from './subtract.js'
+import { validate } from './validate.js'
 
 /**
  * 特定の文字で囲われていることを表すパーサーを返す
@@ -7,35 +11,19 @@ import type { Parser, ParserOutput } from '../types/index.js'
 export const enclose =
 	<T, U>(enclosure: Parser<U>) =>
 		(parser: Parser<T>): Parser<[U, T, U]> =>
-			({ input, position = 0 }) => {
-				const preOutput = enclosure({ input, position })
-
-				if (preOutput.type === 'Failure') {
-					return preOutput
-				}
-
-				const innerOutput = parser({ input, position: preOutput.state.position })
-
-				if (innerOutput.type === 'Failure') {
-					return {
-						...innerOutput,
-						state: { input, position }
-					}
-				}
-
-				const postOutput = enclosure({ input, position: innerOutput.state.position })
-
-				if (postOutput.type === 'Failure' || postOutput.value !== preOutput.value) {
-					return {
-						type: 'Failure',
-						reason: 'Not bracketed by the same character.',
-						state: { input, position }
-					}
-				}
-
-				return {
-					type: 'Success',
-					value: [preOutput.value, innerOutput.value, postOutput.value],
-					state: postOutput.state
-				} satisfies ParserOutput<[U, T, U]>
-			}
+			pipe(
+				seqParser(
+					enclosure,
+					pipe(
+						parser,
+						subtract(enclosure)
+					),
+					enclosure
+				),
+				validate(
+					({ value: [prefix, _, postfix] }) =>
+						prefix === postfix
+							? true
+							: { reason: `expecting prefix and postfix to be the same` }
+				)
+			)
